87d67fd5c0b1b40af04a5e0bb1cf9536419d59e8
Antonin Hildebrand <antonin@hildebrand.cz>
2018-01-24 22:16:44 +0100

    use simple cd instead of pushd/popd

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,7 +4,7 @@ source "$(dirname "${BASH_SOURCE[0]}")/_config.sh" || true || source _config.sh 
 
 mkdir -p "$REPOS_DIR"
 
-pushd "$REPOS_DIR"
+cd "$REPOS_DIR"
 
 if [[ ! -d "trezor-core" ]]; then
   git clone --recursive https://github.com/trezor/trezor-core.git
@@ -19,5 +19,3 @@ fi
 
 pip3 install --no-cache-dir pyblake2
 brew install scons sdl2 sdl2_image
-
-popd
--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -2,7 +2,7 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/_config.sh" || true || source _config.sh # never executes, this is here just for IntelliJ Bash support to understand our sourcing
 
-pushd "$DOCKER_DIR"
+cd "$DOCKER_DIR"
 
 docker run \
   --name "$DOCKER_CONTAINER_NAME" \
@@ -10,5 +10,3 @@ docker run \
   --rm \
   -it "$DOCKER_IMAGE_NAME" \
   "$@"
-
-popd
