8c78e3f711afcb20c38999eceac8a8ae0d55319a
Antonin Hildebrand <antonin@hildebrand.cz>
2018-01-25 01:03:18 +0100

    we need to overlay repos/trezor-core/build

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -4,9 +4,13 @@ source "$(dirname "${BASH_SOURCE[0]}")/_config.sh" || true || source _config.sh 
 
 cd "$DOCKER_DIR"
 
+# we need to overlay repos/trezor-core/build so that mac builds don't clash with unix-y builds inside the container
+# see https://stackoverflow.com/a/38601156/84283 for build dir override
 docker run \
   --name "$DOCKER_CONTAINER_NAME" \
-  -v "$ROOT_DIR:/trezor-mac-toolkit" \
+  --network=host \
+  --mount "type=bind,source=$ROOT_DIR,destination=/trezor-mac-toolkit" \
+  --mount "type=volume,source=trezor-core-build,destination=/trezor-mac-toolkit/repos/trezor-core/build" \
   --rm \
   -it "$DOCKER_IMAGE_NAME" \
   "$@"
