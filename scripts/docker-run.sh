ebe88a9dfdb2b57b14fd90aa18e3b7ff87dd74f7
Antonin Hildebrand <antonin@hildebrand.cz>
2018-05-28 21:29:03 +0200

    set SDL_VIDEODRIVER=dummy when running commands inside docker

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -6,11 +6,14 @@ cd "$DOCKER_DIR"
 
 # we need to overlay repos/trezor-core/build so that mac builds don't clash with unix-y builds inside the container
 # see https://stackoverflow.com/a/38601156/84283 for build dir override
+# SDL_VIDEODRIVER=dummy is needed to work around "Failed to connect to the Mir Server" errors,
+# because SDL doesn't have GUI inside docker container
 docker run \
   --name "$DOCKER_CONTAINER_NAME" \
   --network=host \
   --mount "type=bind,source=$ROOT_DIR,destination=/trezor-mac-toolkit" \
   --mount "type=volume,source=trezor-core-build,destination=/trezor-mac-toolkit/repos/trezor-core/build" \
   --rm \
+  -e SDL_VIDEODRIVER=dummy \
   -it "$DOCKER_IMAGE_NAME" \
   "$@"
